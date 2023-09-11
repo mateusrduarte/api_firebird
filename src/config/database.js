c253809ae54293565ec1c3700041a756189c12cf
Mateus Duarte <mateusrduarte@gmail.com>
2023-09-11 10:14:27 -0300

    Justeando a API para se conectar a um outro banco de dados.

--- src/config/database.js
+++ src/config/database.js
@@ -3,9 +3,9 @@ import Firebird from "node-firebird";
 const dbOptions = {
     host: '127.0.0.1',
     port: 3050,
-    database: 'E:\\Documentos\\Firebird\\BANCONODE.FDB',
+    database: 'C:\\Users\\mateus.duarte\\Documents\\Firebirb\\BANCONODE',
     user: 'SYSDBA',
-    password: 'R@&%64t3',
+    password: 'masterkey',
     lowercase_keys: false, // set to true to lowercase keys
     role: null,            // default
     pageSize: 4096,        // default when creating database
